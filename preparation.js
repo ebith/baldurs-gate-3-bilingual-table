05e9813769d2463ca41d8573591d2860953bc1b2
ebith <ebith.h@gmail.com>
2024-01-12 14:47:32 +0000

    update preparation script

--- preparation.js
+++ preparation.js
@@ -1,40 +1,95 @@
-import { XMLParser } from 'fast-xml-parser'
+import Database from 'better-sqlite3'
 import fs from 'node:fs/promises'
 
-const options = { ignoreDeclaration: true, ignoreAttributes: false }
-const jaXml = await fs.readFile('./japanese.xml')
-const enXml = await fs.readFile('./english.xml')
+const list = new Map()
 
-console.log(`ETA: ${new Date(Date.now() + 10 * 60 * 1000)}`)
+const db_go = new Database('bg3data-gameobjects.sqlite3', { readonly: true, fileMustExist: true })
+for (const row of db_go
+  .prepare('SELECT DisplayNameEnglish, DisplayNameJapanese FROM GameObjects WHERE DisplayNameJapanese IS NOT NULL')
+  .iterate()) {
+  list.set(row.DisplayNameEnglish, [row.DisplayNameEnglish, row.DisplayNameJapanese])
+}
+db_go.close()
 
-const parser = new XMLParser(options)
-const ja = parser.parse(jaXml).contentList.content
-const en = parser.parse(enXml).contentList.content
+const db_raw = new Database('bg3data-raw.sqlite3', { readonly: true, fileMustExist: true })
+const tables = ['Armors', 'Interrupts', 'Objects', 'Passives', 'Spells', 'Statuses', 'Tags', 'Weapons']
+for (const table of tables) {
+  for (const row of db_raw
+    .prepare('SELECT DisplayNameEnglish, DisplayNameJapanese FROM ' + table + ' WHERE DisplayNameJapanese IS NOT NULL')
+    .iterate()) {
+    list.set(row.DisplayNameEnglish, [row.DisplayNameEnglish, row.DisplayNameJapanese])
+  }
+}
+db_raw.close()
 
-const table = [
-  ...new Map(
-    ja
-      .flatMap((e1) => {
-        if (e1['#text'].length > 30 || /(%%% ?Empty|%%% ?Deprecated|。|、|<i>|<b>)/i.test(e1['#text'])) {
-          return []
-        } else {
-          const hit = en.find((e2) => {
-            return e1['@_contentuid'] === e2['@_contentuid']
-          })
-          if (hit) {
-            return {
-              en: hit['#text'],
-              ja: e1['#text'],
-            }
-          } else {
-            return []
-          }
-        }
-      })
-      .map((e) => {
-        return [e.ja, e]
-      }),
-  ).values(),
+const db_loca = new Database('bg3data-localization.sqlite3', { readonly: true, fileMustExist: true })
+for (const row of db_loca
+  .prepare(
+    'SELECT ContentEnglish, ContentJapanese FROM Localization WHERE ContentJapanese IS NOT NULL AND Rootfile LIKE ? OR Rootfile LIKE ?',
+  )
+  .iterate('%_Subregions.lsf.lsx', 'Waypointshrines%')) {
+  list.set(row.ContentEnglish, [row.ContentEnglish, row.ContentJapanese])
+}
+db_loca.close()
+
+const db_journal = new Database('bg3data-journal.sqlite3', { readonly: true, fileMustExist: true })
+for (const row of db_journal
+  .prepare('SELECT QuestTitleEnglish, QuestTitleJapanese FROM Journal WHERE QuestTitleJapanese IS NOT NULL')
+  .iterate()) {
+  list.set(row.QuestTitleEnglish, [row.QuestTitleEnglish, row.QuestTitleJapanese])
+}
+db_journal.close()
+
+const db_feats = new Database('bg3data-feats.sqlite3', { readonly: true, fileMustExist: true })
+for (const row of db_feats
+  .prepare('SELECT DisplayNameEnglish, DisplayNameJapanese FROM Feats WHERE DisplayNameJapanese IS NOT NULL')
+  .iterate()) {
+  list.set(row.DisplayNameEnglish, [row.DisplayNameEnglish, row.DisplayNameJapanese])
+}
+db_feats.close()
+
+const tests = [
+  'Reithwin Town',
+  'Decrepit Village',
+  'Emerald Grove',
+  'Ravaged Beach',
+  'The Amulet of Lost Voices',
+  'Thermoarcanic Gloves',
+  'Boots of Uninhibited Kushigo',
+  'Barbarian',
+  'Paladin',
+  'Acid Splash',
+  'Animal Friendship',
+  'Aid',
+  'Animate Dead',
+  'Banishment',
+  'Arcane Gate',
+  'Mage Slayer',
+  'Polearm Master',
+  'Ability Improvement',
+  'Ritual Caster',
+  'A is for Azuth, and other Gods VI',
+  "Scratch's Ball",
+  'Gheris Hhune',
+  'Entharl Danthelon',
+  'Gerringothe Thorm',
+  'Adamantine Backlash',
+  'Cleansing Touch',
+  'Grasp of the Forest',
+  'Mental Interference',
+  "Shapeshifter's Boon",
+  'Weakened Constitution',
+  'Avenge the Hag Survivors',
+  'Destroy the Ancient Tome',
+  'Feed the Mind Flayer',
+  'Resolve the Abduction',
+  "The Hellion's Heart",
 ]
 
-await fs.writeFile('./src/table.json', JSON.stringify(table))
+for (const test of tests) {
+  if (!list.has(test)) {
+    console.log(`${test}: not found`)
+  }
+}
+
+await fs.writeFile('./src/table.json', JSON.stringify([...list.values()]))
--- src/footer.jsx
+++ src/footer.jsx
@@ -1,10 +1,11 @@
 export function Footer() {
   return (
-    <footer className="text-right">
-      <p>
-        連絡先: <a href="https://github.com/ebith/baldur-s-gate-3-bilingual-table">GitHub</a>,{' '}
+    <footer className="flex">
+      <div className="w-3/5">バージョン: 2023/01/09 Hotfix #16</div>
+      <div className="w-2/5 text-right">
+        連絡先: <a href="https://github.com/ebith/baldurs-gate-3-bilingual-table">GitHub</a>,{' '}
         <a href="https://twitter.com/ebith">Twitter</a>
-      </p>
+      </div>
     </footer>
   )
 }
