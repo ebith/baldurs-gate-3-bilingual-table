b2ac195ed68bb716fb31f3b3a1bcc9aca433e3d8
ebith <ebith.h@gmail.com>
2024-05-19 01:35:23 +0000

    update data

--- src/footer.jsx
+++ src/footer.jsx
@@ -1,7 +1,7 @@
 export function Footer() {
   return (
     <footer className="flex">
-      <div className="w-3/5">バージョン: 2023/01/09 Hotfix #16</div>
+      <div className="w-3/5">2024-04-10 Hotfix #25</div>
       <div className="w-2/5 text-right">
         連絡先:{' '}
         <a href="https://github.com/ebith/baldurs-gate-3-bilingual-table" className="text-orange-600 hover:underline">
